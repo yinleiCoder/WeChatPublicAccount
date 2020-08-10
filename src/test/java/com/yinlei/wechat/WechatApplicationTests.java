a884a7f2125ffaf5675fa57eb90db5c6005f3737
YinLei <37959310+LeiLeiOfJava@users.noreply.github.com>
2020-08-10 16:49:09 +0800

    添加README.md

---
+++ src/test/java/com/yinlei/wechat/WechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yinlei.wechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
